622137a631f70be2c9511e00f4bac530623c6bd1
saksham687 <sakshammalik333@gmail.com>
2022-08-08 18:06:05 +0530

    Create Single Number.cpp

---
+++ ALGO I/Day 14/Single Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums.length == 1)
+            return nums[0];
+        for(int i=1; i < nums.length; i++)
+        {
+            nums[0] = nums[0]^nums[i];
+        }
+        return nums[0];
+    }
+}
