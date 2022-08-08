c65f821b22c9c73890a154be878111ce3d2c0c68
saksham687 <sakshammalik333@gmail.com>
2022-08-08 23:52:58 +0530

    Create design hashmap.cpp

---
+++ DS II/Day 2/design hashmap.cpp
@@ -0,0 +1,48 @@
+class MyHashMap {
+public:
+    vector<list<pair<int,int>>> hm;
+    int siz;
+    MyHashMap() {
+        siz=1000;
+        hm.resize(siz);
+    }
+    
+    int hash(int key){
+        return key%siz;
+    }
+    
+    list<pair<int,int>>::iterator search(int key){
+        int i=hash(key);
+        list<pair<int,int>>::iterator it=hm[i].begin();
+        while(it != hm[i].end()){
+            if(it->first == key)
+                return it;
+            it++;
+        }
+        return it;
+    }
+    
+    void put(int key, int value) {
+        int i=hash(key);
+        list<pair<int,int>>::iterator it=search(key);
+        if(it != hm[i].end())
+            it->second=value;
+        else
+            hm[i].push_back({key,value});
+    }
+    
+    int get(int key) {
+        int i=hash(key);
+        list<pair<int,int>>::iterator it=search(key);
+        if(it != hm[i].end())
+            return it->second;
+        return -1;
+    }
+    
+    void remove(int key) {
+        int i=hash(key);
+        list<pair<int,int>> :: iterator it=search(key);
+        if(it != hm[i].end())
+            hm[i].erase(it);
+    }
+};
